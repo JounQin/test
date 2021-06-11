67b4404e38b745a9ecbdecd04bbebe9351507751
JounQin <admin@1stg.me>
2021-06-11 12:37:25 +0800

    chore: add basic configs

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">{{ message }}</div>
+</template>
+<script lang="ts" setup>
+const message = 'Hello World'
+</script>
+<style lang="scss">
+.container {
+  background-color: red;
+}
+</style>
---
+++ src/shimd.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+
+  const Component: DefineComponent
+
+  export default Component
+}

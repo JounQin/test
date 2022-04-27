131c446c084c88e2c2d2d14c792a6e340398ec18
JounQin <admin@1stg.me>
2022-04-27 16:51:09 +0800

    chore: reproduce incorrect loc of tokens

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+// @ts-check
+
 import fs from 'fs/promises'
 
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkMdx from 'remark-mdx'
-import { visit } from 'unist-util-visit'
 
-const processor = unified().use(remarkParse).use(remarkMdx)
+/** @type Array.<import('acorn').Token> */
+const tokens = []
+
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkMdx, {
+    acornOptions: {
+      ecmaVersion: 2020,
+      onToken: tokens,
+    },
+  })
 
 const main = async () => {
-  const root = processor.parse(await fs.readFile('test.mdx', 'utf8'))
-
-  visit(
-    root,
-    node => node.data?.estree.comments?.length,
-    node => {
-      console.log(JSON.stringify(node.data.estree.comments, null, 2))
-    },
-  )
+  processor.parse(await fs.readFile('test.mdx', 'utf8'))
+  console.log(JSON.stringify(tokens, null, 2))
 }
 
 main()
